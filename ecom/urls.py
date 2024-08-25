481f6e205fa6704a4293156b0545cec33e3c6eaa
EstebanDev <esteban.villasecatt@gmail.com>
2024-08-25 18:08:05 -0400

    adding the project  base templates

--- ecom/urls.py
+++ ecom/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from ecom import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('store.urls')),
+    path('', include('store.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_DOOR)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
