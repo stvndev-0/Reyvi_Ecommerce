48c2e7116c6c9187a1d030f1730cbca5d8b2331d
EstebanDev <esteban.villasecatt@gmail.com>
2024-09-02 15:42:04 -0400

    adding slug to urls

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,6 @@ from .views import HomeListView, ProductDetailView, CategoryListView
 
 urlpatterns = [
     path('', HomeListView.as_view(), name='home'),
-    path('product/<int:pk>', ProductDetailView.as_view(), name='product-detail'),
+    path('product/<slug:slug>', ProductDetailView.as_view(), name='product-detail'),
     path('category/<slug:slug>', CategoryListView.as_view(), name='category')
 ]
