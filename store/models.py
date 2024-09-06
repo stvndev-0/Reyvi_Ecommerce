fb0e44540c401b6cb9bf64b7e831709dea09cfa5
EstebanDev <esteban.villasecatt@gmail.com>
2024-09-06 18:22:18 -0400

    modified data type

--- store/models.py
+++ store/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
 from django.core.exceptions import ValidationError
+from decimal import Decimal, ROUND_HALF_UP
 
 # Create your models here.
 class Category(models.Model):
@@ -21,10 +22,10 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True, blank=True)
+    slug = models.SlugField(max_length=100, unique=True, blank=True)
     description = models.CharField(max_length=250, blank=True, null=True)
     image = models.ImageField(upload_to='product/')
-    price = models.IntegerField(default=0)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=10)
     is_sale = models.BooleanField(default=False)
     discount_percentage = models.IntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
@@ -37,7 +38,9 @@ class Product(models.Model):
 
     @property
     def discounted_price(self):
-        return int(self.price * (1 - self.discount_percentage / 100))
+        discount = Decimal(1) - (Decimal(self.discount_percentage) / Decimal(100))
+        discounted_price = self.price * discount
+        return discounted_price.quantize(Decimal('0.01'), ROUND_HALF_UP)
     
     def save(self, *args, **kwargs):
         if not self.slug:
