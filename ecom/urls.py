37941b30fe726727f2d2b3693390a20b63c966a6
EstebanDev <esteban.villasecatt@gmail.com>
2024-09-20 20:09:02 -0300

    Change of names in the views

--- ecom/urls.py
+++ ecom/urls.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from ecom import settings
 from django.conf.urls.static import static
-from users.views import LoginView, RegisterView
+from users.views import LogInView, SignUpView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
     path('my_account/', include('users.urls')),
-    path('login/', LoginView.as_view(), name='login'),
-    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LogInView.as_view(), name='login'),
+    path('signup/', SignUpView.as_view(), name='signup'),
     path('cart/', include('cart.urls')),
     path('payment/', include('payment.urls'))
 ]
