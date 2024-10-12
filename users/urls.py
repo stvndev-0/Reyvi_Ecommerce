c2e0c3173f6a7f193aa3171c2222c8331f6528f8
EstebanDev <esteban.villasecatt@gmail.com>
2024-10-11 23:30:29 -0300

    rename in urls

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from .views import AccountListView, ManageAccountView, AccountUserUpdateView, ShippingAddressView, ShippingAddressCreateView,ShippingAddressUpdateView
+from .views import AccountDetailView, ManageAccountView, AccountUserUpdateView, ShippingAddressView,ShippingAddressUpdateView, OrderView
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('', AccountListView.as_view(), name='my_account'),
+    path('', AccountDetailView.as_view(), name='my_account'),
     path('manage_account/', ManageAccountView.as_view(), name='manage_account'),
     path('manage_account/account_user_update/', AccountUserUpdateView.as_view(), name='user_update'),
     path('address_book/', ShippingAddressView.as_view(), name='address_book'),
-    path('address_book/shippingAddress_create/', ShippingAddressCreateView.as_view(), name='address_create'),
-    path('address_book/shippingAddress_update/<int:pk>', ShippingAddressUpdateView.as_view(), name='address_update'),
+    path('address_book/new_address/', ShippingAddressView.as_view(), name='new_address'),
+    path('address_book/update_address/<int:pk>/', ShippingAddressUpdateView.as_view(), name='update_address'),
+    path('order/', OrderView.as_view(), name='my_order'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
