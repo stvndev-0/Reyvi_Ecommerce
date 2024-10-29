76aed40009610a2f67232cf0b5bf3a68cc5a3826
EstebanDev <esteban.villasecatt@gmail.com>
2024-10-29 17:57:57 -0300

    The name of the "my_account" URL was changed and the inventory URLs were added

--- ecom/urls.py
+++ ecom/urls.py
@@ -2,16 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from ecom import settings
 from django.conf.urls.static import static
-from users.views import LogInView, SignUpView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
-    path('my_account/', include('users.urls')),
-    path('login/', LogInView.as_view(), name='login'),
-    path('signup/', SignUpView.as_view(), name='signup'),
+    path('accounts/', include('users.urls')),
     path('cart/', include('cart.urls')),
-    path('payment/', include('payment.urls'))
+    path('payment/', include('payment.urls')),
+    path('inventory/', include('inventory.urls')),
 ]
 
 if settings.DEBUG:
