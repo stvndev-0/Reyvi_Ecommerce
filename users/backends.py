b3a157b69cb97939fe065c1d4b97397804e90759
EstebanDev <esteban.villasecatt@gmail.com>
2024-10-29 13:13:08 -0300

    Added a backends file for email validations and more

---
+++ users/backends.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model
+from django.db.models import Q
+
+User = get_user_model()
+
+class EmailOrUsernameBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            # Intentar buscar el usuario por email
+            user = User.objects.get(Q(username=username) | Q(email=username))
+        except User.DoesNotExist:
+            return None
+        else:
+            # Verificar la contraseña
+            if user.check_password(password):
+                return user
+        return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
