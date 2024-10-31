ab98db13ab56cd791202cac2dd6fe5d8ad4dce9e
EstebanDev <esteban.villasecatt@gmail.com>
2024-10-31 19:41:00 -0300

    minimal changes

--- users/forms.py
+++ users/forms.py
@@ -2,10 +2,9 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-from django.contrib.auth import authenticate
 
 class LoginForm(AuthenticationForm):
-	username = forms.EmailField(label="Email", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
+	username = forms.CharField(label="Email", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
 	password = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
 
 
